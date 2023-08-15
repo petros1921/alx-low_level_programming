e6182e016fdc21a25f4e99b54f16e507bb83a81c
petros1921 <petrosworku19@gmail.com>
2023-08-15 02:54:13 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void ptint_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
