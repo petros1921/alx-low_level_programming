51b546453e9308bcbcc9ea43caf325162a710a8b
petros1921 <petrosworku19@gmail.com>
2023-08-17 08:49:29 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_linr - Draws a straight line according to parameter
+ * print_line - Draws a straight line according to parameter
  * @n: The number of lines to draw
  * Return: empty
  */
