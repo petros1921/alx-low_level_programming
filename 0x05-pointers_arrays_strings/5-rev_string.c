0d3e838e110f81f16ef861ba7ca9801cb6904606
petros1921 <petrosworku19@gmail.com>
2023-08-21 01:01:40 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,7 +8,7 @@
 
 void rev_string(char *s)
 {
-	int len = 0, index =0;
+	int len = 0, index = 0;
 	char tmp;
 
 	while (s[index++])
@@ -17,7 +17,7 @@ void rev_string(char *s)
 	for (index = len - 1; index >= len / 2; index--)
 	{
 	tmp = s[index];
-	s[index] = s [len - index - 1];
-	s [len - index - 1] = tmp;
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
 	}
 }
