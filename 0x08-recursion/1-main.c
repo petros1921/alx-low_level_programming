4b8af6e92ea6e76cedec27930409a42a887e20d1
petros1921 <petrosworku19@gmail.com>
2023-08-29 22:59:22 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -2,11 +2,12 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 
 int main(void)
 {
-    _puts_recursion("Puts with recursion");
+    _print_rev_recursion("\nColton Walker");
     return (0);
 }
