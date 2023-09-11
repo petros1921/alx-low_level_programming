9029c46b56b18167b9a9c42ccd0cc9b106bbf622
petros1921 <petrosworku19@gmail.com>
2023-09-11 02:17:19 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  * Return: the length of the string
  */
 
-int _strlen(char*s)
+int _strlen(char *s)
 {
 	int i;
 
@@ -32,7 +32,7 @@ int _strlen(char*s)
  * Return: the pointer to dest
  */
 
-char*_strcpy(char*dest, char *src)
+char*_strcpy(char *dest, char *src)
 {
 	int len, i;
 
@@ -63,7 +63,7 @@ char*_strcpy(char*dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t*dog;
+	dog_t *dog;
 	int len1, len2;
 
 	len1 = _strlen(name);
@@ -73,14 +73,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char)*(len1 + 1));
+	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
-
 		return(NULL);
 	}
-	dog->owner = malloc(sizeof(char)*(len2 + 1));
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
