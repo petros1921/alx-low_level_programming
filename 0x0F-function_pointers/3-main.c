d7189ef117d0aabd04da0d95d3c33d5b504e180b
petros1921 <petrosworku19@gmail.com>
2023-09-13 04:25:23 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,7 +6,7 @@
 /**
  * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * @argc: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
@@ -24,7 +24,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	op = argv[2];
-	num2 = atoi(argv[3]);
+	num2 = atoi (argv[3]);
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
