ecd98ca4964d997900003f7b4652ca2c12def5d8
petros1921 <petrosworku19@gmail.com>
2023-09-13 04:04:55 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,7 +4,7 @@
 /**
  * main - prints its own opcodes
  * @argc: number of arguments
- * @arguv: array of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx", arr[i]);
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
