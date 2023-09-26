aa4ce8bd54eaec31c18e173abbdf6110a2773532
peter1921 <petrosworku@gmail.com>
2023-09-26 01:32:34 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: linked lisr of type listint_t to print
+ * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
  */
